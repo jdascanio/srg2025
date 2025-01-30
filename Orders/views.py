7df0252abc5ac35e5fe25154fff240503ba67751
Julian Positron <gulator@gmail.com>
2025-01-30 00:04:00 -0300

    neworder html R0

--- Orders/views.py
+++ Orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from Orders.models import *
+from Products.models import *
+from Users.models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def neworder (request):
+    return render (request, 'neworder.html')
--- srg/urls.py
+++ srg/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',users_views.login_request, name='login_request'),
     path('Users/', include('Users.urls')),
-    path('Products/', include('Products.urls'))
-    # path('Orders/', include('Orders.urls')),
+    path('Products/', include('Products.urls')),
+    path('Orders/', include('Orders.urls')),
 ]
